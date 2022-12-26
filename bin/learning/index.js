257171039bbec0fc7718280fbcb6c05b28fdf082
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-26 09:59:24 -0400

    feat: Added automation modify

---
+++ bin/learning/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+const { uniqueNamesGenerator, adjectives, names } = require('unique-names-generator')
+const path = require('path')
+
+const generateRandomNumber = () => Math.trunc(Math.random()*1000)
+const generateRandomName = () => uniqueNamesGenerator({
+  dictionaries: [adjectives, names],
+  length: 2
+})
+
+const pushToMain = () => {
+  exec('git add .')
+  exec(`git commit -m "feat: Added atom of Issue #${generateRandomNumber()}"`)
+  exec('git push origin main')
+}
+
+const modifyFile = (path) => {
+  if (!fs.existsSync(path)) return
+  const fileContent = fs.readFileSync(path, 'utf8')
+  fs.writeFileSync(path, fileContent + `\n@${generateRandomName()}`)
+}
+
+setInterval(() => {
+  console.log('--- Modify file.')
+  modifyFile(path.join(__dirname, './text.txt'))
+  console.log('--- Do push to main branch')
+}, 10000)
